b16cd4460afb83baf63b4c02b4e4b261de2387d9
filipemedeiros12 <filipemedeiros144@gmail.com>
2021-07-24 11:44:32 -0300

    opa-dois

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Opa meu jovem </Text>
+      <Text style={styles.text}>Opa meu jovem! </Text>
+      <Image style={styles.image} source={require("./assets/flamengo.png")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +19,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text:{
+    color:'red',
+    
+  },
+  image:{
+  width: 200,
+  height: 200,
+  }
 });
